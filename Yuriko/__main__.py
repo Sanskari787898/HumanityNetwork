c30660da2e5acbab551192788505fc45146a1349
Sanskari787898 <93301854+Sanskari787898@users.noreply.github.com>
2022-01-25 17:09:30 +0530

    Update __main__.py

--- Yuriko/__main__.py
+++ Yuriko/__main__.py
@@ -90,7 +90,7 @@ PM_START_TEXT = """
 × *𝐔𝐩𝐓𝐢𝐦𝐞:* `{}`
 × `{}` *𝐔𝐬𝐞𝐫, 𝐀𝐜𝐫𝐨𝐬𝐬* `{}` *𝐂𝐡𝐚𝐭𝐬.*
 ────────────────────────
-✗ *𝐏𝐨𝐰𝐞𝐫𝐞𝐝 💕 𝐁𝐲: 𝗗𝗲𝘃𝗶𝗟 𝗛𝗮𝗰𝗸𝗲𝗥 @JaiHindChatting !*
+✗ *𝐏𝐨𝐰𝐞𝐫𝐞𝐝 💕 𝐁𝐲: 𝗗𝗲𝘃𝗶𝗟 𝗛𝗮𝗰𝗸𝗲𝗥 @HUMANITYPOWER_SUPPORT !*
 """
 
 buttons = [
@@ -99,9 +99,9 @@ buttons = [
             text="𝐇𝐞𝐥𝐩 & 𝐂𝐨𝐦𝐦𝐚𝐧𝐝𝐬", callback_data="help_back"),
     ],
     [
-        InlineKeyboardButton(text="𝐌𝐮𝐬𝐢𝐜", callback_data="yurikorobot_asst"),
+        InlineKeyboardButton(text="𝐒𝐎𝐔𝐑𝐂𝐄", url=f"https://github.com/rakeshyt/TrishaXManager"),
         InlineKeyboardButton(
-            text="𝐒𝐮𝐩𝐩𝐨𝐫𝐭", url=f"https://t.me/JaiHindChatting"
+            text="𝐒𝐮𝐩𝐩𝐨𝐫𝐭", url=f"https://t.me/HUMANITYPOWER_SUPPORT"
         ),
     ],
     [
@@ -111,7 +111,7 @@ buttons = [
         ),
     ],
     [
-        InlineKeyboardButton(text="𝐀𝐝𝐝 𝐓𝐫𝐢𝐬𝐡𝐚 𝐈𝐧 𝐆𝐫𝐨𝐮𝐩", url="http://t.me/DevilTrishaRoBot?startgroup=true"),
+        InlineKeyboardButton(text="𝐀𝐝𝐝 𝐇𝐔𝐌𝐀𝐍𝐈𝐓𝐘 𝐈𝐧 𝐆𝐫𝐨𝐮𝐩", url="http://t.me/HUMANITYPOWER_BOT?startgroup=true"),
     ],
 ]
 
@@ -248,7 +248,7 @@ def start(update: Update, context: CallbackContext):
             ),
             parse_mode=ParseMode.HTML,
             reply_markup=InlineKeyboardMarkup(
-                [[InlineKeyboardButton(text="𝐒𝐨𝐮𝐫𝐜𝐞 𝐀𝐧𝐝 𝐒𝐮𝐩𝐩𝐨𝐫𝐭", url="t.me/JaiHindChatting")]]
+                [[InlineKeyboardButton(text="𝐒𝐨𝐮𝐫𝐜𝐞 𝐀𝐧𝐝 𝐒𝐮𝐩𝐩𝐨𝐫𝐭", url="t.me/HUMANITYPOWER_SUPPORT")]]
             ),
         )
         
@@ -380,8 +380,8 @@ def yurikorobot_about_callback(update, context):
     query = update.callback_query
     if query.data == "yurikorobot_":
         query.message.edit_text(
-            text=""" *𝐓𝐫𝐢𝐬𝐡𝐚* - `A bot to manage your groups with additional features!`
-            \n`Here the basic help regarding use of 𝐓𝐫𝐢𝐬𝐡𝐚𝐑𝐨𝐛𝐨𝐭.`
+            text=""" *⚡️𝐇𝐔𝐌𝐀𝐍𝐈𝐓𝐘 𝐍𝐄𝐓𝐖𝐎𝐑𝐊⚡️* - `A bot to manage your groups with additional features!`
+            \n`Here the basic help regarding use of ⚡️𝐇𝐔𝐌𝐀𝐍𝐈𝐓𝐘 𝐍𝐄𝐓𝐖𝐎𝐑𝐊⚡️.`
             
             \n`Almost all modules usage defined in the help menu, checkout by sending` `/help`
             \n`Report error/bugs click the Button`""",
@@ -391,10 +391,10 @@ def yurikorobot_about_callback(update, context):
                 [
                     [
                         InlineKeyboardButton(
-                            text="𝐁𝐮𝐠'𝐬", url="t.me/JaiHindChatting"
+                            text="𝐁𝐮𝐠'𝐬", url="t.me/HUMANITYPOWER_SUPPORT"
                         ),
                         InlineKeyboardButton(
-                            text="𝐂𝐡𝐚𝐧𝐧𝐞𝐥", url="https://t.me/RymOfficial"
+                            text="𝐂𝐡𝐚𝐧𝐧𝐞𝐥", url="https://t.me/HUMANTIPOWERBOT_SUPPORT"
                         ),
                     ],
                     [InlineKeyboardButton(text="Back", callback_data="yurikorobot_back")],
@@ -421,7 +421,7 @@ def yurikorobot_about_callback(update, context):
             
             f"\n\n✗ `Firstly Add` {dispatcher.bot.first_name} `to your group by pressing` [here](http://t.me/{dispatcher.bot.username}?startgroup=true)\n"
             f"\n✗ `After adding promote me manually with full rights for faster experience.`\n"
-            f"\n✗ `Than send` `/admincache@DevilTrishaRoBot` `in that chat to refresh admin list in My database.`\n"
+            f"\n✗ `Than send` `/admincache` `in that chat to refresh admin list in My database.`\n"
             f"\n\n*All done now use below given button's to know about use!*\n"
             f"",
             parse_mode=ParseMode.MARKDOWN,
@@ -447,7 +447,7 @@ def yurikorobot_about_callback(update, context):
         query.message.edit_text(
             text=f"*Let's Make Your Group Bit Effective Now*"
             
-            f"\n✗ `Congragulations, 𝐓𝐫𝐢𝐬𝐡𝐚𝐑𝐨𝐛𝐨𝐭 now ready to manage your group.`"
+            f"\n✗ `Congragulations, ⚡️𝐇𝐔𝐌𝐀𝐍𝐈𝐓𝐘 𝐍𝐄𝐓𝐖𝐎𝐑𝐊⚡️ now ready to manage your group.`"
             f"\n\n*Admin Tools*"
             f"\n✗ `Basic Admin tools help you to protect and powerup your group.`"
             f"\n✗ `You can ban members, Kick members, Promote someone as admin through commands of bot.`"
@@ -482,7 +482,7 @@ def yurikorobot_about_callback(update, context):
             f"\n\n✗ `2.) then promote me as admin and give all permissions except anonymous admin.`"
             f"\n\n✗ `3.) Assistant Auto Join to your group:`"
             f"\n\n✗ `4.) turn on the video chat first before start to play music.`"
-            f"\n\n✗ *Lets Enjoy The 𝐓𝐫𝐢𝐬𝐡𝐚 𝐌𝐮𝐬𝐢𝐜 And Join Support Group @JaiHindChatting*"
+            f"\n\n✗ *Lets Enjoy The ⚡️𝐇𝐔𝐌𝐀𝐍𝐈𝐓𝐘 𝐍𝐄𝐓𝐖𝐎𝐑𝐊⚡️ 𝐌𝐮𝐬𝐢𝐜 And Join Support Group @HUMANITYPOWER_SUPPORT*"
             f"\n\n*✗ 𝐏𝐨𝐰𝐞𝐫𝐞𝐝 💕 𝐁𝐲: 𝗗𝗲𝘃𝗶𝗟 𝗛𝗮𝗰𝗸𝗲𝗥*",
             parse_mode=ParseMode.MARKDOWN,
             disable_web_page_preview=True,
@@ -494,7 +494,7 @@ def yurikorobot_about_callback(update, context):
         query.message.edit_text(
             text=f"*Let's Make Your Group Bit Effective Now*"
             
-            f"\n✗ `Congragulations, 𝐓𝐫𝐢𝐬𝐡𝐚𝐑𝐨𝐛𝐨𝐭 now ready to manage your group.`"
+            f"\n✗ `Congragulations, ⚡️𝐇𝐔𝐌𝐀𝐍𝐈𝐓𝐘 𝐍𝐄𝐓𝐖𝐎𝐑𝐊⚡️ now ready to manage your group.`"
             f"\n\n*Admin Tools*"
             f"\n✗ `Basic Admin tools help you to protect and powerup your group.`"
             f"\n✗ `You can ban members, Kick members, Promote someone as admin through commands of bot.`"
@@ -509,17 +509,17 @@ def yurikorobot_about_callback(update, context):
         )    
     elif query.data == "yurikorobot_support":
         query.message.edit_text(
-            text="*𝐓𝐫𝐢𝐬𝐡𝐚 𝐒𝐮𝐩𝐩𝐨𝐫𝐭 𝐂𝐡𝐚𝐭𝐬*"
+            text="*⚡️𝐇𝐔𝐌𝐀𝐍𝐈𝐓𝐘 𝐍𝐄𝐓𝐖𝐎𝐑𝐊⚡️ 𝐒𝐮𝐩𝐩𝐨𝐫𝐭 𝐂𝐡𝐚𝐭𝐬*"
             
             "\n\n✗ `Join Support Group/Channel`",
             parse_mode=ParseMode.MARKDOWN,
             reply_markup=InlineKeyboardMarkup(
                 [
                  [
-                    InlineKeyboardButton(text="𝐒𝐨𝐮𝐫𝐜𝐞 𝐀𝐧𝐝 𝐒𝐮𝐩𝐩𝐨𝐫𝐭", url="t.me/JaiHindChatting"),
+                    InlineKeyboardButton(text="𝐒𝐨𝐮𝐫𝐜𝐞 𝐀𝐧𝐝 𝐒𝐮𝐩𝐩𝐨𝐫𝐭", url="t.me/HUMANITYPOWER_SUPPORT"),
                  ],
                  [
-                    InlineKeyboardButton(text="𝐋𝐨𝐠𝐬", url="t.me/THEDRAGONV6"),
+                    InlineKeyboardButton(text="𝐋𝐨𝐠𝐬", url="https://t.me/HUMANTIPOWERBOT_SUPPORT"),
                     InlineKeyboardButton(text="𝐔𝐩𝐝𝐚𝐭𝐞𝐬", url="https://t.me/RymOfficial"),
                  ],
                  [
@@ -531,9 +531,9 @@ def yurikorobot_about_callback(update, context):
         )
     elif query.data == "yurikorobot_credit":
         query.message.edit_text(
-            text=f"<b> CREDIT FOR 𝐓𝐑𝐈𝐒𝐇𝐀 DEV'S</b>\n"
+            text=f"<b> CREDIT FOR ⚡️𝐇𝐔𝐌𝐀𝐍𝐈𝐓𝐘 𝐍𝐄𝐓𝐖𝐎𝐑𝐊⚡️ DEV'S</b>\n"
             
-            f"\n`✗ Here Some Developers Helping in Making The 𝐓𝐫𝐢𝐬𝐡𝐚 Bot`",
+            f"\n`✗ Here Some Developers Helping in Making The ⚡️𝐇𝐔𝐌𝐀𝐍𝐈𝐓𝐘 𝐍𝐄𝐓𝐖𝐎𝐑𝐊⚡️ Bot`",
             parse_mode=ParseMode.HTML,
             reply_markup=InlineKeyboardMarkup(
                 [
@@ -554,7 +554,7 @@ def Source_about_callback(update, context):
     query = update.callback_query
     if query.data == "source_":
         query.message.edit_text(
-            text=""" Hi..😻 I'm *𝗧𝗿𝗶𝘀𝗵𝗮𝗥𝗼𝗯𝗼𝘁*
+            text=""" Hi..😻 I'm *⚡️𝐇𝐔𝐌𝐀𝐍𝐈𝐓𝐘 𝐍𝐄𝐓𝐖𝐎𝐑𝐊⚡️*
                  \nHere is the [🔥Source Code🔥](https://github.com/rakeshyt) .""",
             parse_mode=ParseMode.MARKDOWN,
             disable_web_page_preview=True,
